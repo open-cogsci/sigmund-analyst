93517ddbf5473fedd6ced49158ed9505debebfb9
smathot <s.mathot@cogsci.nl>
2025-04-11 12:04:24 +0200

    Language selection: remap invalid lexer names to valid aliases

--- pyqt_code_editor/syntax_highlighters/syntax_highlighter.py
+++ pyqt_code_editor/syntax_highlighters/syntax_highlighter.py
@@ -4,10 +4,18 @@ from pygments.lexers import get_lexer_by_name
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
 module_cache = {}
+# Some lexers have names that are not recognized by get_lexer_by_name(), which
+# seems like an issue with pygments. To catch this, here we explicitly rename.
+LANGUAGE_MAP = {
+    'javascript+genshi text': 'javascript+genshi'
+}
 
     
-def create_syntax_highlighter(language, *args, **kwargs):
-    try:
+def create_syntax_highlighter(language, *args, **kwargs):    
+    if language in LANGUAGE_MAP:
+        logger.info(f'mapping {language} to {LANGUAGE_MAP[language]}')
+        language = LANGUAGE_MAP[language]
+    try:        
         lexer = get_lexer_by_name(language)
     except:
         lexer = get_lexer_by_name('markdown')
