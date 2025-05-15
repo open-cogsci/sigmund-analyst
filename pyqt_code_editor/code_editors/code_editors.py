5bc3c3bd25faf3282f35be95dcb0184546cfc1ec
smathot <s.mathot@cogsci.nl>
2025-05-15 14:23:02 +0200

    Implement language map to handle synonymous languages - Bump to 0.0.8

--- pyqt_code_editor/code_editors/code_editors.py
+++ pyqt_code_editor/code_editors/code_editors.py
@@ -6,6 +6,11 @@ logger = logging.getLogger(__name__)
 
 editor_module_cache = {}
 
+# Some languages should be treated as synonyms
+LANGUAGE_MAP = {
+    'ipython': 'python'
+}
+
     
 def create_editor(path=None, language=None, *args, **kwargs):
     if language is None:
@@ -13,6 +18,7 @@ def create_editor(path=None, language=None, *args, **kwargs):
             language = settings.default_language
         else:
             language = utils.guess_language_from_path(path)
+            language = LANGUAGE_MAP.get(language, language)
     # Load the editor module depending on the language. We store the
     # imported module in a cache for efficiency
     if language not in editor_module_cache:
