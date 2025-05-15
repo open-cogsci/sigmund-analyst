9a0f0bfbb8d01b1895775de259fc500ad4406020
smathot <s.mathot@cogsci.nl>
2025-05-15 14:26:16 +0200

    Upgrade when installing through script

--- install_sigmund_analyst.sh
+++ install_sigmund_analyst.sh
@@ -26,7 +26,7 @@ echo "▶ Creating virtual-env in $VENV_DIR ..."
 echo "▶ Installing package ..."
 source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
-pip install pyqt_code_editor
+pip install --upgrade pyqt_code_editor
 deactivate
 
 echo "▶ Creating wrapper in $WRAPPER ..."
