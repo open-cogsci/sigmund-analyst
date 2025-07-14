444ee62e07741ec3060d371642809ddca2a06ac5
smathot <s.mathot@cogsci.nl>
2025-07-14 15:37:51 +0200

    Specify name for Sigmund

--- pyqt_code_editor/components/sigmund.py
+++ pyqt_code_editor/components/sigmund.py
@@ -98,7 +98,7 @@ class Sigmund(Dock):
         super().__init__('Sigmund', parent)
         self.setObjectName("sigmund")
         workspace = EditorWorkspace(editor_panel)
-        self.sigmund_widget = SigmundWidget(self)
+        self.sigmund_widget = SigmundWidget(self, application='Sigmund Analyst')
         self.sigmund_widget.setStyleSheet(f'font-size: {settings.font_size}pt')
         self.sigmund_widget.set_workspace_manager(workspace)
         self.setWidget(self.sigmund_widget)
